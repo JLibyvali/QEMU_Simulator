5f45dad04569e42b5230447d00a64b0f22bc6bd7
Jlibyvali <1954925228@qq.com>
2025-02-18 22:19:49 +0800

    Modify build kernel

--- QEMU/clean.sh
+++ QEMU/clean.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
+set -e
+
 current=$(pwd)
 uboot=$current/uboot
 kernel=$current/kernel
 
 if [ -d "$uboot" ];then
-    echo " remove uboot files " 
+    echo " remove uboot files "
     rm -r $uboot
 fi
 
-if [ -d "$kernel" ];then 
+if [ -d "$kernel" ];then
     echo " remove kernel files "
     rm -r $kernel
 fi
 
-echo " clean all " 
+echo " clean all "
--- QEMU/uboot_smode.sh
+++ QEMU/uboot_smode.sh
@@ -1,24 +1,47 @@
 #!/bin/sh
 
+set -e
+
 current=$(pwd)
 QEMUOPTS= -machine virt -cpu rv32 -smp 2 -m 128M -nographic -kernel $current/uboot/u-boot.bin
 
-if ! command -v qemu-system-riscv32 &> /dev/null ;then
+check_qemu()
+{
     
-    echo " qemu-system-riscv32 not found! " 
-    exit 1
+    if ! command -v qemu-system-riscv32 &> /dev/null ;then
+        echo " qemu-system-riscv32 not found! "
+        exit 1
+    fi
+    
+}
 
-else
 
-# qemu-system-riscv32: virt target https://www.qemu.org/docs/master/system/riscv/virt.html, using qemu deafult OpenSBI as bios
+boot_uboot_smode(){
+    
+    # qemu-system-riscv32: virt target https://www.qemu.org/docs/master/system/riscv/virt.html, using qemu deafult OpenSBI as bios
     
     echo "==> Boot Uboot in riscv Core S-mode "
     
-    if test  -f $current/uboot/u-boot.bin ;then
-
-            qemu-system-riscv32 $QEMUOPTS
-    else 
-            echo "==X u-bot.bin not found! " 
-            exit 1
+    if test  -f $current/uboot/u-boot.bin ; then
+        
+        qemu-system-riscv32 $QEMUOPTS
+    else
+        echo "==X u-bot.bin not found! "
+        exit 1
     fi
-fi 
+}
+
+
+
+case $1 in
+    -h | --help)
+        echo "$0 -uboot         \" Boot U-boot in directly to S-mode. \" "
+    ;;
+    -uboot)
+        check_qemu()
+        boot_uboot_smode()
+    ;;
+    *)
+        echo "$0 -h[--help] for look help list."
+    ;;
+esac
